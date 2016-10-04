2c06d9fa7356a3c700bf1104e4f8daf9cc41297c
Aaper <aram.parsegyan@gmail.com>
2016-10-04 16:06:24 +0200

    Create upload-apk.sh

---
+++ upload-apk.sh
@@ -0,0 +1,20 @@
+#create a new directory that will contain out generated apk
+mkdir $HOME/buildApk/ 
+#copy generated apk from build folder to the folder just created
+cp -R app/build/outputs/apk/app-debug.apk $HOME/android/
+#go to home and setup git
+cd $HOME
+git config --global user.email "aram.parsegyan@gmail.com"
+git config --global user.name "Aaper" 
+#clone the repository in the buildApk folder
+git clone --quiet --branch=master  https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA  master > /dev/null
+#go into directory and copy data we're interested
+cd master  cp -Rf $HOME/android/* .
+#add, commit and push files
+git add -f .
+git remote rm origin
+git remote add origin https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA.git
+git add -f .
+git commit -m "[skip ci] Travis build $TRAVIS_BUILD_NUMBER pushed"
+git push -fq origin master > /dev/null
+echo -e "Donen"
