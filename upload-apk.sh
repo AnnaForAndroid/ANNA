d636adf3f32af4fb34124adf346b614c37cdab72
Aaper <aram.parsegyan@gmail.com>
2016-10-06 13:51:28 +0200

    try

--- upload-apk.sh
+++ upload-apk.sh
@@ -10,11 +10,8 @@ git config --global user.name "Aaper"
 #clone the repository in the buildApk folder
 git clone --quiet https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA
 #go into directory and copy data we're interested
-cd master  cp -Rf $HOME/buildApk/* .
+cp -Rf $HOME/buildApk/* ./ANNA/
 #add, commit and push files
-git add -f .
-git remote rm origin
-git remote add origin https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA.git
 git add -f .
 git commit -m "[skip ci] Travis build $TRAVIS_BUILD_NUMBER pushed"
 git push -fq origin master > /dev/null
