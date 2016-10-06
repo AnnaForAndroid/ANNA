bb797b6250cdf3a966410753963718668afb0728
Aaper <aram.parsegyan@gmail.com>
2016-10-06 13:18:16 +0200

    try

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,13 +1,14 @@
 #create a new directory that will contain out generated apk
-mkdir $HOME/buildApk/ 
+mkdir $HOME/buildApk/
+mkdir $HOME/repo/ 
 #copy generated apk from build folder to the folder just created
 cp -R ./ANNA/app/build/outputs/apk/app-debug.apk $HOME/buildApk/
 #go to home and setup git
-cd $HOME
+cd $HOME/repo
 git config --global user.email "aram.parsegyan@gmail.com"
 git config --global user.name "Aaper" 
 #clone the repository in the buildApk folder
-git clone --quiet --branch=master  https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA  master > /dev/null
+git clone --quiet https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA
 #go into directory and copy data we're interested
 cd master  cp -Rf $HOME/buildApk/* .
 #add, commit and push files
