1fa75d0b8b056e01b638901c991256c8e6157544
Aaper <aram.parsegyan@gmail.com>
2016-10-06 15:23:44 +0200

    use all apks

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,21 +1,16 @@
 #create a new directory that will contain out generated apk
 mkdir $HOME/buildApk/
-mkdir $HOME/repo/ 
 #copy generated apk from build folder to the folder just created
-cp -R ./ANNA/app/build/outputs/apk/app-debug.apk $HOME/buildApk/
+cp -R ./ANNA/app/build/outputs/apk/* $HOME/buildApk/
 #go to home and setup git
-cd $HOME/repo
+cd $HOME
 git config --global user.email "aram.parsegyan@gmail.com"
 git config --global user.name "Aaper" 
 #clone the repository in the buildApk folder
-echo -e "clone Project"
 git clone --quiet https://Aaper:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNA
 #go into directory and copy data we're interested
-echo -e "clone done"
-cd $HOME/repo/ANNA
-cp -Rf $HOME/buildApk/* .
+cp -Rf $HOME/buildApk/* ./ANNA/
 #add, commit and push files
 git add -f .
 git commit -m "[skip ci] Travis build $TRAVIS_BUILD_NUMBER pushed"
 git push
-echo -e "Donen"
