d71ce62d5605f7c6e2e7b3399058edecf63a9311
Digister <digister@protonmail.com>
2016-11-21 10:53:21 +0100

    Refactored chat view

--- ANNA/app/src/main/java/com/anna/chat/ChatViewActivity.java
+++ ANNA/app/src/main/java/com/anna/chat/ChatViewActivity.java
@@ -123,7 +123,7 @@ public class ChatViewActivity extends Fragment {
         if (hotwordDetection.getUserAnswer().equalsIgnoreCase(getString(R.string.yes))) {
             voiceOutput.read(notificationData.getText().toString());
             voiceOutput.read(getString(R.string.ask_to_answer));
-            if (hotwordDetection.getUserAnswer().toLowerCase().equals(getString(R.string.yes))) {
+            if (hotwordDetection.getUserAnswer().equalsIgnoreCase(getString(R.string.yes))) {
                 //   voice.promptSpeechInput();
                 //   answerMessage(notificationData, voice.getVoiceInput());
             }
