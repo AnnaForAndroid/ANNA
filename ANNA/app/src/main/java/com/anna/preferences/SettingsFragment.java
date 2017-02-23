a89ba75145474ff44b86c1d31473be048b650460
Digister <digister@protonmail.com>
2017-02-23 10:52:06 +0100

    Preperation for Settingscreen enhancement

---
+++ ANNA/app/src/main/java/com/anna/preferences/SettingsFragment.java
@@ -0,0 +1,10 @@
+package com.anna.preferences;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by D062427 on 20.02.2017.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+}
